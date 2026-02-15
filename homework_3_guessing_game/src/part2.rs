549f6a8cadcb5ec5b01a2163753b541ff4597036
George <gwyatt@bu.edu>
2026-02-15 18:57:02 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -6,36 +6,30 @@ pub struct Part2 {}
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        let mut num: u32 = max/2;
+        // If max is exclusive, the valid search interval is [min, max-1].
+        // Handle empty range first.
+       
+        let mut low = min;
+        let mut high = max - 1; 
 
-        loop {
-        let guess_status = player.ask_to_compare(num);
+        while low <= high {
+            let num = low + (high - low) / 2; 
+            let guess_status = player.ask_to_compare(num);
 
-        if guess_status == 0 {
-            return num
-
-    
-        } else if guess_status == 1 {
-            if num < max-1 {
-                num = num + 1;
+            if guess_status == 0 {
+                return num;
+            } else if guess_status == -1 {
+                if num == 0 {
+                    break;
+                }
+                high = num - 1;
+            } else if guess_status == 1 {
+                low = num + 1;
             } else {
-                return max-1
+                break;
             }
-            
-        } else if guess_status == -1 {
-            if num > min {
-                num = num - 1;
-            } else {
-                return min   
-            }
-        
-        } else {
-            return num
         }
 
-            }
-
-            
-        }
+        min
     }
-
+}
